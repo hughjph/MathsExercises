72c1a30fbf632b10f0c5b8b27cbcbe79278e6d1f
hugjph <hughjohnson2018@outlook.com>
2023-02-21 18:28:48 +0000

    initial commit/problem1 week 2@

---
+++ problem1.py
@@ -0,0 +1,28 @@
+a = [int(i) for i in input("Input number A: ")]
+b = int(input("Input number B: "))
+
+q_array = []
+r = 0
+
+for i in range(len(a)):
+    count = 0
+    while a[i] >= b:
+        a[i] -= b
+        count += 1
+
+    if i == (len(a) - 1):
+        r = a[i]
+        q_array.append(count)
+    else:
+        a[i+1] = int(str(a[i]) + str(a[i+1]))
+        q_array.append(count)
+
+
+q = 0
+
+for i in range(len(q_array)):
+    q = q*10 + q_array[i]
+
+
+print("Quotient of A divided by B: " + str(q))
+print("Remainder of A divided by B: " + str(r))
